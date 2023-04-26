221de8e02a121378d0d20358247634b624e63d40
JorgeUndurraga <jorgeundurraga@yahoo.com>
2023-04-25 23:52:36 -0400

    cambios ingreso de datos y cálculos

--- script.js
+++ script.js
@@ -1,58 +1,123 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+//$(document).ready(function(){
+
+
 // <----------------- INGRESAR PRESUPUESTO---------------------------->
 let cantidadPresupuesto = 0;
-displayPresupuesto.innerHTML = cantidadPresupuesto.toLocaleString('es-CL', {
-    type: 'currency',
-    currency: 'CLP',
-    });
+displayPresupuesto.innerHTML = cantidadPresupuesto.toLocaleString("es-CL", {type: "currency", currency: "CLP",});
 
-formularioPresupuesto.addEventListener("submit", (event)=>{
+formularioPresupuesto.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  cantidadPresupuesto = itemPresupuesto.value;
+  displayPresupuesto.innerHTML = parseInt(cantidadPresupuesto).toLocaleString( "es-CL",{type: "currency", currency: "CLP",}
+  );
+});
+
+// <----------------- CREACION DE OBJETO---------------------------->
+// creacion Objeto
+function ItemGasto(gasto, valor, id) {
+  this.gasto = gasto;
+  this.valor = valor;
+  this.id = id;
+}
+
+
+
+// Creación del array para la tabla
+formularioGasto.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log(itemPresupuesto.value)
+  
+    let descripcionGasto = itemNombreGasto.value;
+    let valorGasto = itemCantidadGasto.value;
+    let idGasto = uuidv4().slice(0, 6);
 
-    cantidadPresupuesto = itemPresupuesto.value
-    displayPresupuesto.innerHTML = parseInt(cantidadPresupuesto).toLocaleString('es-CL', {
-        type: 'currency',
-        currency: 'CLP',
-        });;
-})
+    let arraydeItemGasto = creadorArrayGasto(descripcionGasto, valorGasto, idGasto);
 
-// <----------------- INGRESAR GASTO---------------------------->
-function ItemGasto(id, gasto, valor){
-    this.id=id;
-    this.gasto=gasto;
-    this.valor=valor
-} 
+    console.log(arraydeItemGasto)//nooooooooo
 
-formularioGasto.addEventListener("submit", (event)=>{
-    event.preventDefault();
-    console.log(itemNombreGasto.value)
-    console.log(itemCantidadGasto.value)
+    despliegueDeGastos(arraydeItemGasto);
 
-    let arrayGastos;
+    function despliegueDeGastos(arraydeItemGasto){
+      let gastosTotales = sumadeGastos(arraydeItemGasto);
+      displayGastos.innerHTML  = gastosTotales;
+      displaySaldo.innerHTML = cantidadPresupuesto - gastosTotales;
+    }
 
-    // arrayDigimon.forEach((x, index) => {
-    //     acumulador += `
-    //             <tr>
-    //                 <td>${index + 1}</td>
-    //                 <td>${x.name}</td>
-    //                 <td>${x.level}</td>
-    //                 <td class="col text-center"><button class="btn btn-primary" onclick="functionMostrarModal('${
-    //                   x.img
-    //                 }', '${x.name}')" >Imagen </button></td>
-    //             </tr>
-    //         `;
-    //   });
+// Rellenado de la  tabla de Gastos
+let rellenarGastos = () => {
+        // declaración de la variables par el relleno de las tablas
+        tableBody.innerHTML = "";
+        let acumulador = "";
+        // recorrido de la tabla e instalación de los datos en ella
+        arraydeItemGasto.forEach((e) => {
 
-      tableBody.innerHTML = acumulador;
+          console.log(e);
+
+            acumulador += `
+            <tr id ="${e.id}">
+                <td>${e.gasto}</td>
+                <td>${e.valor}</td>
+                <td><button><i class="bi bi-trash3-fill borrarTd"></button></i></td>
+            </tr> `;
+        });
+        tableBody.innerHTML = acumulador;
+        // <td><input onclick="borrarFila();"><i class="bi bi-trash3-fill borrarFila"></i></td>
+
+          $(document).on('click', '.borrarTd', function(event) {
+            event.preventDefault();
+            event.stopImmediatePropagation(); //para que no repeta laccion sucesivamente
+            
+            let idTr = $(this).closest('tr').attr('id');
+
+            var borrandoDato = function(idTr, arraydeItemGasto) {
+              let arrayRestado = arraydeItemGasto.filter((item)=>item.id !== idTr)
+              return arrayRestado;
+            };
+
+          arraydeItemGastoRenovado = borrandoDato(idTr, arraydeItemGasto);
+
+          $(this).closest('tr').remove();
+
+          console.log(arraydeItemGastoRenovado)// esta la lleva
+          despliegueDeGastos(arraydeItemGastoRenovado);
+
+          });
+
+    };
+
+    rellenarGastos();
+
+});
+
+// ---------------------------------FUNCIONES--------------------------------
+
+let arrayDeGastos = [];
+let gastoNuevo =[];
+// creador de array
+    function creadorArrayGasto(descripcionGasto, valorGasto, idGasto) {
+      gastoNuevo = new ItemGasto(descripcionGasto, valorGasto, idGasto);
+      arrayDeGastos.push(gastoNuevo);
+      console.log(arrayDeGastos)
+      return arrayDeGastos;
+    }
+
+
+// suma de gastos
+function sumadeGastos(arraydeItemGasto){
+    let  suma = 0;
+    arraydeItemGasto.forEach((e) => {
+    suma += parseInt(e.valor);
     });
+    return suma;
+}
 
-    // cantidadPresupuesto = itemPresupuesto.value
-    // displayPresupuesto.innerHTML = parseInt(cantidadPresupuesto).toLocaleString('es-CL', {
-    //     type: 'currency',
-    //     currency: 'CLP',
-    //     }
-        
-        
-})
+
+//})
+
+
+
+
 
 
